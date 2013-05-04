801626cc3d966d968773d35f867b0e0c3c9521e8
Ken Lu <ken@seeice.com>
2013-05-04 13:43:17 -0400

    init project with default framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueSchedulerDemo::Application.config.secret_token = '724dabc16325bc5e2f822912bca9111f2cc93af9d957865bb59a75721f7dc3e6898071cc633d7d34e54ad2657d1b7d0c0c554ce2b514aad0b7171bc0f5e2e3b0'
